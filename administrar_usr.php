dd7df87573cef31ceefd466b0a1aa866c03df351
DantorKing <hd8afdez@hotmail.com>
2020-12-17 17:29:39 -0600

    Formas de admin para usuario y productos

---
+++ administrar_usr.php
@@ -0,0 +1,113 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Administrar Usuarios</title>
+        <!--<link rel="stylesheet" href="estilo.css">-->
+        <script>
+            document.addEventListener("DOMContentLoaded",() => {
+                
+            });
+        </script>
+    </head>
+    <style>
+        body{
+                text-align: center;
+                font-size:20;
+            }
+        input[type="submit"]{
+                padding: 10px;
+                color: #fff;
+                background: #003ce1;
+                width: 290px;
+                margin: 20px auto;
+                margin-top: 0;
+                border:0;
+                border-radius: 0;
+                cursor: pointer;
+            }
+        table{
+                border: 1px solid black;
+                font-size: 25;
+            }
+        td{
+                border:1px solid black;
+            }
+        th{
+                text-align: left;
+            }
+        a{
+                position: absolute;
+                top: 850px;
+                left: 940px;
+            }
+    </style>
+    <body>
+        <h1>Usuarios</h1>
+        <h3>Borrar</h3>
+        <form method="post" action="adminusr.php"><br>
+            <table style="position: absolute; top:110px; left:810px">
+                <tr>
+                    <td>Correo Electrónico:</td> <td><input type="text" name="nombre"></td><br>
+                </tr>
+                
+            </table>
+                <input type="submit" name="borrar" value="Eliminar Usuario">
+        </form>
+        
+        <h3>Agregar o Actualizar</h3>
+        <form method="post" action="adminusr.php"><br>
+            <table style="position: absolute; top:260px; left:672px">
+            <tr>
+                <td>Nombre actual: </td><td><input type="text" name="nombre"></td><br>
+            </tr>
+            <tr>
+                <td>Apellidos actuales: </td><td><input type="text" name="apellidos_act"></td><br>
+            </tr>
+            
+            <tr>
+                <td>Nuevo nombre:</td><td> <input type="text" name="nuevo" required></td><br>
+            </tr>
+            <tr>
+                <td>Nuevos apellidos:</td><td> <input type="text" name="apellidos_new" required></td><br>
+            </tr>
+            <tr>
+                <td>E-Mail:</td><td> <input type="text" name="email"></td><br>
+            </tr>
+            <tr>
+                <td>Fecha de Nacimiento: </td><td><input type="date" name="fecha_nac"></td><br>
+            </tr>
+            <tr>
+                <td>Teléfono:</td><td> <input type="text" name="telefono"></td><br>
+            </tr>
+            <tr>
+                <td>Gustos:</td><td><textarea cols='30' rows='8' name='gustos'></textarea></td><br>
+            </tr>
+            <th>Dirección</th>
+            <tr>
+                <td>Calle:</td><td> <input type="text" name="calle"></td><br>
+            </tr>
+            <tr>
+                <td>Colonia:</td><td> <input type="text" name="colonia"></td><br>
+            </tr>
+            <tr>
+                <td>Ciudad:</td><td> <input type="text" name="ciudad"></td><br>
+            </tr>
+            <tr>
+                <td>Estado:</td><td> <input type="text" name="estado"></td><br> 
+            </tr>
+            <tr>
+                <td style="align-self: center"><input type="submit" name="actualizar" value="Actualizar Usuario"></td>
+                <td style="align-self: center"><input type="submit" name="agregar" value="Agregar Usuario"></td>
+            </tr>
+            </table>
+        </form>
+
+            
+            <a href="administrar.php">Regresar</a>
+    </body>
+</html>
--- formaRegistro.php
+++ formaRegistro.php
@@ -4,25 +4,90 @@
 <html>
     <head>
         <style>
+           input[type="submit"]{
+                position: absolute;
+                padding: 10px;
+                color: #fff;
+                background: #003ce1;
+                width: 300px;
+                margin: 20px auto;
+                margin-top: 0;
+                border:0;
+                border-radius: 0;
+                cursor: pointer;
+                top: 650px;
+                left: 820px;
+            }
+            h2{
+                text-align: center;
+            }
+            table{
+                position: absolute;
+                top: 100px; 
+                left: 720px;
+                border: 1px solid black;
+                font-size: 25;
+            }
+            td{
+                border:1px solid black;
+            }
+            th{
+                text-align: left;
+            }
+            body{
+                text-align: center;
+                font-size:20;
+            }
+            a{
+                position: absolute;
+                top: 700px;
+                left: 950px;
+            }
         </style>
         <meta charset="UTF-8">
         <title>Registrar</title>
     </head>
     <body>
         <form action="registro.php" method="post">
-            <h3>Datos Generales</h3>
-            Nombre: <input type="text" name="nombre"><br>
-            Apellidos: <input type="text" name="apellidos"><br>
-            Contraseña: <input type="password" name="pwd"><br>
-            Fecha de Nacimiento: <input type="date" name="fecha_nac"><br>
-            E-Mail: <input type="text" name="email"><br>
-            Teléfono: <input type="text" name="telefono"><br>
-            Gustos: <br><textarea cols='80' rows='8' name='gustos'></textarea><br>
-            <h3>Dirección</h3>
-            Calle: <input type="text" name="calle"><br>
-            Colonia: <input type="text" name="colonia"><br>
-            Ciudad: <input type="text" name="ciudad"><br>
-            Estado: <input type="text" name="estado"><br><br>
+            <!--<table border="1" align="center">-->
+            <table align="center">
+                <th>Datos Generales</th>
+                <tr>
+                <td>Nombre:</td> <td><input type="text" name="nombre"></td><br>
+                </tr>
+                <tr>
+                    <td>Apellidos:</td> <td><input type="text" name="apellidos"></td><br>
+                </tr>
+		<tr>
+	    	    <td>Contraseña:</td> <td><input type="password" name="pwd"></td><br>
+		</tr>
+                <tr>
+                    <td>Fecha de Nacimiento:</td> <td><input type="date" name="fecha_nac"></td><br>
+                </tr>
+                <tr>
+                    <td>E-Mail:</td> <td><input type="text" name="email"></td><br>
+                </tr>
+                <tr>
+                    <td>Teléfono:</td> <td><input type="text" name="telefono"></td><br>
+                </tr>
+                <tr>
+                    <td>Gustos:</td> <td><textarea cols='30' rows='5' name='gustos'></textarea></td><br>
+                </tr>
+            
+                <th>Dirección</th>
+            <tr>
+                <td>Calle:</td> <td><input type="text" name="calle"></td><br>
+            </tr>
+            <tr>
+                <td>Colonia:</td> <td><input type="text" name="colonia"></td><br>
+            </tr>
+            <tr>
+            <td>Ciudad:</td> <td><input type="text" name="ciudad"></td><br>
+            </tr>
+            <tr>
+            <td>Estado:</td> <td><input type="text" name="estado"></td><br><br>
+            </tr>
+            </table><br>
             <input type="submit" name="registro" value="Registrar">
         </form>
         <p><a href="index.php">Inicio</a></p>
